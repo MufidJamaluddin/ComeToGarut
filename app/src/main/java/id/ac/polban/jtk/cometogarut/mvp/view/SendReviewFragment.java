db10927e86669086fc0c5f45a29eacde03116a89
MufidJamaluddin <mufid.jamaluddin@gmail.com>
2018-07-21 21:12:15 +0700

    Memperbaiki dan Melengkapi Send Review dan Send Suggestion

--- app/src/main/java/id/ac/polban/jtk/cometogarut/mvp/view/SendReviewFragment.java
+++ app/src/main/java/id/ac/polban/jtk/cometogarut/mvp/view/SendReviewFragment.java
@@ -7,6 +7,7 @@ import android.support.design.widget.Snackbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.RatingBar;
 
 import id.ac.polban.jtk.cometogarut.R;
@@ -14,15 +15,27 @@ import id.ac.polban.jtk.cometogarut.mvp.contract.SendReviewPlaceContract;
 import id.ac.polban.jtk.cometogarut.mvp.model.Review;
 import id.ac.polban.jtk.cometogarut.mvp.presenter.SendReviewPlacePresenter;
 
+/**
+ * Fragment untuk mengirimkan Review User
+ */
 public class SendReviewFragment extends BaseFragment implements SendReviewPlaceContract.View
 {
     private RatingBar securityRating;
     private RatingBar cleanlinessRating;
     private RatingBar facilityRating;
     private RatingBar orderlyRating;
+    private EditText nameField;
+    private EditText emailField;
 
     private SendReviewPlacePresenter presenter;
 
+    /**
+     * Membuat View dlm ragment
+     * @param inflater : inflanter view
+     * @param container : viewgroup
+     * @param savedInstanceState : state
+     * @return view
+     */
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
@@ -40,6 +53,9 @@ public class SendReviewFragment extends BaseFragment implements SendReviewPlaceC
 
         this.orderlyRating = view.findViewById(R.id.rating_orderly);
 
+        this.nameField = view.findViewById(R.id.user_name);
+
+        this.emailField = view.findViewById(R.id.user_email);
         /*
          * Klik Tombol Kirim
          */
@@ -54,12 +70,13 @@ public class SendReviewFragment extends BaseFragment implements SendReviewPlaceC
                  */
                 Review review = new Review();
 
-                //review.setName(...);
-                //review.setEmail(...);
+                review.setName(String.valueOf(nameField.getText()));
+                review.setEmail(String.valueOf(emailField.getText()));
                 review.setPlace_id(getPlaceId().toString());
                 review.setSecurity_rate(String.valueOf(securityRating.getRating()));
                 review.setPolicy_rate(String.valueOf(orderlyRating.getRating()));
                 review.setPurity_rate(String.valueOf(cleanlinessRating.getRating()));
+                review.setFacility_rate(String.valueOf(facilityRating.getRating()));
 
                 /*
                  * Kirim Review ke Server
@@ -71,6 +88,9 @@ public class SendReviewFragment extends BaseFragment implements SendReviewPlaceC
         return view;
     }
 
+    /**
+     * Menghancurkan View dlm Fragment
+     */
     @Override
     public void onDestroyView()
     {
--- app/src/main/java/id/ac/polban/jtk/cometogarut/mvp/view/SendSuggestionFragment.java
+++ app/src/main/java/id/ac/polban/jtk/cometogarut/mvp/view/SendSuggestionFragment.java
@@ -3,16 +3,38 @@ package id.ac.polban.jtk.cometogarut.mvp.view;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import id.ac.polban.jtk.cometogarut.R;
 import id.ac.polban.jtk.cometogarut.mvp.contract.SendSuggestionPlaceContract;
+import id.ac.polban.jtk.cometogarut.mvp.model.Suggestion;
+import id.ac.polban.jtk.cometogarut.mvp.presenter.SendSuggestionPlacePresenter;
 
+/**
+ * Fragment untuk Mengirimkan Masukkan/Saran (SUGGESTION)
+ */
 public class SendSuggestionFragment extends BaseFragment implements SendSuggestionPlaceContract.View
 {
+    private EditText nameField;
+    private EditText emailField;
+    private EditText descriptionField;
+    private Button sendSuggestionBtn;
 
+    private SendSuggestionPlacePresenter presenter;
+
+    /**
+     * Membuat View dlm Fragment
+     *
+     * @param inflater : inflanter layout
+     * @param container : viewgroup
+     * @param savedInstanceState : state
+     * @return view
+     */
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
@@ -20,31 +42,41 @@ public class SendSuggestionFragment extends BaseFragment implements SendSuggesti
         View view;
         view = inflater.inflate(R.layout.fragment_send_suggestion, container, false);
 
+        this.presenter = new SendSuggestionPlacePresenter();
+        this.presenter.attach(this);
+
+        this.nameField = view.findViewById(R.id.user_name);
+        this.emailField = view.findViewById(R.id.user_email);
+        this.descriptionField = view.findViewById(R.id.user_description);
+
+        this.sendSuggestionBtn = view.findViewById(R.id.sh_suggestion);
+
+        this.sendSuggestionBtn.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                Suggestion suggestion = new Suggestion();
+                //
+                suggestion.setName(String.valueOf(nameField.getText()));
+                suggestion.setEmail(String.valueOf(emailField.getText()));
+                suggestion.setDescription(String.valueOf(descriptionField.getText()));
+                // Kirim Suggestion
+                presenter.sendSuggestion(suggestion);
+            }
+        });
+
         return view;
     }
 
+    /**
+     * Menghancurkan View
+     */
     @Override
     public void onDestroyView()
     {
+        this.presenter.detach();
         super.onDestroyView();
-    }
-
-    /**
-     * Menampilkan Loading
-     */
-    @Override
-    public void showLoading()
-    {
-
-    }
-
-    /**
-     * Menyembunyikan Loading
-     */
-    @Override
-    public void hideLoading()
-    {
-
     }
 
     /**
@@ -55,7 +87,8 @@ public class SendSuggestionFragment extends BaseFragment implements SendSuggesti
     @Override
     public void showMessage(String message)
     {
-
+        if(this.getView() != null)
+            Snackbar.make(this.getView(), message, Snackbar.LENGTH_LONG).show();
     }
 
 }
